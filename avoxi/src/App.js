8d9843787ac2a1be350c84a06222c20965535a7e
BigBricks <BSA6.23.94@gmail.com>
2018-11-04 12:47:26 -0500

    initialized and actions mostly done

---
+++ avoxi/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  state ={
+    movies = [],
+    search = ""
+  }
+  render() {
+    return (
+      <div className="App">
+       
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ avoxi/src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+export const FETCHTODO = "FETCHTODO";
+export const ERROR = "ERROR";
+
+export const searchTodo = search => {
+  return dispatch => {
+    axios
+      .get(`http://www.omdbapi.com/?s=${search}&apikey=f86c0e32`)
+      .then(response => {
+        dispatch({ type: FETCHTODO, payload: response.data });
+      })
+      .catch(err => {
+        dispatch({ type: ERROR });
+      });
+  };
+};
